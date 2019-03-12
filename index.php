612f5ca7dfa14a892aff05e7f5ee229453ecbd61
fidele5 <fideleplk@gmail.com>
2019-03-12 08:11:04 +0100

    la finalisation du formulaire

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Election d'Esis</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+			<div class="row"> 
+				<div class="col-md-6">
+
+					<!-- Header -->
+						<div id="header">
+
+							<!-- Logo -->
+                                <h1><a href="ecologie.jpg" id="logo"> <img src="ecologie.jpg" alt="" width="60%"  /></a></h1>
+                                
+							<!-- Nav -->
+								<nav id="nav">
+									<ul>
+										<li class="current"><a href="#">Connexion</a></li>
+										<li class="current"><a href="#">Créer un Compte</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					<!-- CTA -->
+						&copy; Para-Academique d'Esis 2019
+					 
+				</div>
+				<div class="col-md-3">
+				</div>
+			</div>
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
